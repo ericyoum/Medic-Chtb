1cb576066219008d4ee8021be98f44aaa6c5bced
ngaruko <bede.ngaruko@gmail.com>
2022-08-11 14:21:11 +1200

    refactor generic contact form

--- tests/page-objects/forms/generic-contact-form.wdio.page.js
+++ tests/page-objects/forms/generic-contact-form.wdio.page.js
@@ -1,41 +1,40 @@
-const helper = require('../../helper');
+const mutedModalSelector = '.modal-content #contacts-muted';
 
 const openForm = async (formId) => {
-  const addButton = element(by.css('.detail-actions .actions .dropdown-toggle .fa-plus'));
-  await helper.clickElementNative(addButton);
-  const form = element(by.css(`#relevant-contacts-form li[id="form:${formId}"] a`));
-  await helper.clickElementNative(form);
+  const addButton = await $('.detail-actions .actions .dropdown-toggle .fa-plus');
+  await addButton.click();
+  const form = await $(`#relevant-contacts-form li[id="form:${formId}"] a`);
+  await form.click();
 };
-
-const mutedModalSelector = '.modal-content #contacts-muted';
-const mutedModal = element(by.css(mutedModalSelector));
-const modalCancelButton = element(by.css(`${mutedModalSelector} .modal-footer .btn.cancel`));
-const modalAcceptButton = element(by.css(`${mutedModalSelector} .modal-footer .btn.submit`));
 
 module.exports.openForm = async (formId) => {
   await openForm(formId);
-  await helper.waitUntilReadyNative(element(by.id('form-title')));
+  const formTitle = await $('#form-title');
+  await formTitle.waitForDisplayed();
 };
 
 module.exports.submit = async () => {
-  const submitButton = element(by.css('.btn.submit.btn-primary'));
-  await helper.clickElementNative(submitButton);
-  await helper.waitElementToBeVisibleNative(element(by.css('div.row.flex.grid'))); // contact summary loaded
+  const submitButton = await $('.btn.submit.btn-primary');
+  await submitButton.click();
+  const flexGrid = await $(('div.row.flex.grid'));
+  await flexGrid.waitForDisplayed(); // contact summary loaded
 };
 
 module.exports.openFormForMutedContact = async (formId) => {
   await openForm(formId);
   // popup is shown instead of form
-  await helper.waitUntilReadyNative(mutedModal);
+  const mutedModal = await $(mutedModalSelector);
+  await mutedModal.waitForDisplayed();
 };
 
 module.exports.closeModal = async (confirm) => {
+  const modalAcceptButton = await $(`${mutedModalSelector} .modal-footer .btn.submit`);
   if (confirm) {
-    await helper.clickElementNative(modalAcceptButton);
-    await helper.waitUntilReadyNative(element(by.id('form-title')));
+    await modalAcceptButton.click();
+    await (await $('#form-title')).waitForDisplayed();
     return;
   }
-
-  await helper.clickElementNative(modalCancelButton);
-  await browser.sleep(1000); // nothing should happen, but let's wait and make sure no redirection takes place
+  const modalCancelButton = await $(`${mutedModalSelector} .modal-footer .btn.cancel`);
+  await modalCancelButton.click();
+  await browser.pause(1000); // nothing should happen, but let's wait and make sure no redirection takes place
 };
